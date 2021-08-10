6acdd4aebd5b64c8979a6ed9469c6c24c7191a8e
Rafael Escobar Salas <rafael.escobar.s@gmail.com>
2021-08-10 19:34:41 -0400

    two numbers operations

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,40 @@
+//Botones Flowchart
 var flowcharts_button = document.getElementById("flowcharts_button");
 var flowcharts_content = document.getElementById("flowcharts_content");
 var flowcharts_operations = document.getElementById("flowcharts_operations");
 var flowcharts = document.getElementById("flowcharts");
-var suma_button = document.getElementById("suma_button");
+var suma_flowchart_button = document.getElementById("suma_flowchart_button");
 var suma_flowchart = document.getElementById("suma_flowchart");
-var resta_button = document.getElementById("resta_button");
+var resta_flowchart_button = document.getElementById("resta_flowchart_button");
 var resta_flowchart = document.getElementById("resta_flowchart");
-var multiplicacion_button = document.getElementById("multiplicacion_button");
+var multiplicacion_flowchart_button = document.getElementById(
+  "multiplicacion_flowchart_button"
+);
 var multiplicacion_flowchart = document.getElementById(
   "multiplicacion_flowchart"
 );
-var division_button = document.getElementById("division_button");
+var division_flowchart_button = document.getElementById(
+  "division_flowchart_button"
+);
 var division_flowchart = document.getElementById("division_flowchart");
-var modulo_button = document.getElementById("modulo_button");
+var modulo_flowchart_button = document.getElementById(
+  "modulo_flowchart_button"
+);
 var modulo_flowchart = document.getElementById("modulo_flowchart");
 
+//Section Operations
+var operations_button = document.getElementById("operations_button");
+var operations_buttons = document.getElementById("operations_buttons");
+
+var operations_section = document.getElementById("operations_section");
+var resultado = document.getElementById("resultado");
+var suma_operation_button = document.getElementById("suma_operation_button");
+var resta_operation_button = document.getElementById("resta_operation_button");
+var multiplicacion_operation_button = document.getElementById(
+  "multiplicacion_operation_button"
+);
+
+//Boton Flowcharts
 flowcharts_button.onclick = function () {
   if (flowcharts_content.classList.contains("d-none")) {
     flowcharts_content.classList.remove("d-none");
@@ -23,33 +43,30 @@ flowcharts_button.onclick = function () {
     multiplicacion_flowchart.classList.add("d-none");
     division_flowchart.classList.add("d-none");
     modulo_flowchart.classList.add("d-none");
+    operations_section.classList.add("d-none");
+    operations_buttons.classList.add("d-none");
+    resultado.classList.add("d-none");
   } else {
     flowcharts_content.classList.add("d-none");
-    suma_flowchart.classList.add("d-none");
-    resta_flowchart.classList.add("d-none");
-    multiplicacion_flowchart.classList.add("d-none");
-    division_flowchart.classList.add("d-none");
-    modulo_flowchart.classList.add("d-none");
   }
 };
 
-suma_button.onclick = function () {
+//Botón Suma
+suma_flowchart_button.onclick = function () {
   if (suma_flowchart.classList.contains("d-none")) {
     suma_flowchart.classList.remove("d-none");
     resta_flowchart.classList.add("d-none");
     multiplicacion_flowchart.classList.add("d-none");
     division_flowchart.classList.add("d-none");
     modulo_flowchart.classList.add("d-none");
+    resultado.classList.add("d-none");
   } else {
     suma_flowchart.classList.add("d-none");
-    resta_flowchart.classList.add("d-none");
-    multiplicacion_flowchart.classList.add("d-none");
-    division_flowchart.classList.add("d-none");
-    modulo_flowchart.classList.add("d-none");
   }
 };
 
-resta_button.onclick = function () {
+//Botón Resta
+resta_flowchart_button.onclick = function () {
   if (resta_flowchart.classList.contains("d-none")) {
     resta_flowchart.classList.remove("d-none");
     suma_flowchart.classList.add("d-none");
@@ -58,14 +75,11 @@ resta_button.onclick = function () {
     modulo_flowchart.classList.add("d-none");
   } else {
     resta_flowchart.classList.add("d-none");
-    suma_flowchart.classList.add("d-none");
-    multiplicacion_flowchart.classList.add("d-none");
-    division_flowchart.classList.add("d-none");
-    modulo_flowchart.classList.add("d-none");
   }
 };
 
-multiplicacion_button.onclick = function () {
+//Botón Multiplicación
+multiplicacion_flowchart_button.onclick = function () {
   if (multiplicacion_flowchart.classList.contains("d-none")) {
     multiplicacion_flowchart.classList.remove("d-none");
     suma_flowchart.classList.add("d-none");
@@ -74,14 +88,11 @@ multiplicacion_button.onclick = function () {
     modulo_flowchart.classList.add("d-none");
   } else {
     multiplicacion_flowchart.classList.add("d-none");
-    suma_flowchart.classList.add("d-none");
-    resta_flowchart.classList.add("d-none");
-    division_flowchart.classList.add("d-none");
-    modulo_flowchart.classList.add("d-none");
   }
 };
 
-division_button.onclick = function () {
+//Botón División
+division_flowchart_button.onclick = function () {
   if (division_flowchart.classList.contains("d-none")) {
     division_flowchart.classList.remove("d-none");
     suma_flowchart.classList.add("d-none");
@@ -90,14 +101,11 @@ division_button.onclick = function () {
     modulo_flowchart.classList.add("d-none");
   } else {
     division_flowchart.classList.add("d-none");
-    suma_flowchart.classList.add("d-none");
-    resta_flowchart.classList.add("d-none");
-    multiplicacion_flowchart.classList.add("d-none");
-    modulo_flowchart.classList.add("d-none");
   }
 };
 
-modulo_button.onclick = function () {
+//Botón Módulo
+modulo_flowchart_button.onclick = function () {
   if (modulo_flowchart.classList.contains("d-none")) {
     modulo_flowchart.classList.remove("d-none");
     suma_flowchart.classList.add("d-none");
@@ -106,9 +114,97 @@ modulo_button.onclick = function () {
     division_flowchart.classList.add("d-none");
   } else {
     division_flowchart.classList.add("d-none");
-    suma_flowchart.classList.add("d-none");
-    resta_flowchart.classList.add("d-none");
-    multiplicacion_flowchart.classList.add("d-none");
-    modulo_flowchart.classList.add("d-none");
   }
 };
+
+//Botón Operaciones con dos números
+operations_button.onclick = function () {
+  if (operations_section.classList.contains("d-none")) {
+    operations_section.classList.remove("d-none");
+    operations_buttons.classList.remove("d-none");
+    resultado.classList.remove("d-none");
+    flowcharts_content.classList.add("d-none");
+  } else {
+    operations_section.classList.add("d-none");
+    flowcharts_content.classList.add("d-none");
+    operations_buttons.classList.add("d-none");
+    resultado.classList.add("d-none");
+  }
+};
+
+//Evitar que se presionesn teclas que no sean numeros
+function numbers_only(input) {
+  var regex = /[^0-9]/gi;
+  input.value = input.value.replace(regex, "");
+}
+
+//Validación de datos
+function validacion(num1, num2) {
+  if (num1 > 0 && num2 > 0) {
+    if (num1 != num2) {
+    } else {
+      resultado.innerHTML =
+        "Los números ingresados son iguales. Ingrese números distintos.";
+      return false;
+    }
+  } else {
+    resultado.innerHTML =
+      "Los números ingresados no son mayores que cero. Ingrese números mayores que cero.";
+    return false;
+  }
+}
+
+function suma(num1, num2) {
+  if (validacion(num1, num2) == false) {
+  } else {
+    var suma = num1 + num2;
+    if (isNaN(suma) == false) {
+      resultado.innerHTML = "La suma es " + suma;
+      S;
+    }
+  }
+}
+
+function resta(num1, num2) {
+  if (validacion(num1, num2) == false) {
+  } else {
+    var resta = num1 - num2;
+    if (isNaN(resta) == false) {
+      resultado.innerHTML = "La resta es " + resta;
+      S;
+    }
+  }
+}
+
+function multiplicacion(num1, num2) {
+  if (validacion(num1, num2) == false) {
+  } else {
+    var multiplicacion = num1 * num2;
+    if (isNaN(multiplicacion) == false) {
+      resultado.innerHTML = "La multiplicación es " + multiplicacion;
+      S;
+    }
+  }
+}
+
+function division(num1, num2) {
+  if (validacion(num1, num2) == false) {
+  } else {
+    var division = num1 / num2;
+    if (isNaN(division) == false) {
+      resultado.innerHTML = "La división es " + division;
+      S;
+    }
+  }
+}
+
+function modulo(num1, num2) {
+  if (validacion(num1, num2) == false) {
+  } else {
+    var modulo = num1 % num2;
+    if (isNaN(modulo) == false) {
+      resultado.innerHTML = "El módulo es " + modulo;
+      S;
+    }
+  }
+}
