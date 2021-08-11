5365302f6faddc26875bd0bc87e98c8eb8f8acd9
Rafael Escobar Salas <rafael.escobar.s@gmail.com>
2021-08-11 18:21:29 -0400

    5 sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,6 +34,18 @@ var multiplicacion_operation_button = document.getElementById(
   "multiplicacion_operation_button"
 );
 
+//Botones Temperatura
+var temperatura_button = document.getElementById("temperatura_button");
+var temperatura_resultado = document.getElementById("temperatura_resultado");
+
+//Botones Años
+var dias_button = document.getElementById("dias_button");
+var dias_resultado = document.getElementById("dias_resultado");
+
+//Botones Promedio
+var promedio_button = document.getElementById("promedio_button");
+var promedio_resultado = document.getElementById("promedio_resultado");
+
 //Boton Flowcharts
 flowcharts_button.onclick = function () {
   if (flowcharts_content.classList.contains("d-none")) {
@@ -46,6 +58,12 @@ flowcharts_button.onclick = function () {
     operations_section.classList.add("d-none");
     operations_buttons.classList.add("d-none");
     resultado.classList.add("d-none");
+    temperatura_section.classList.add("d-none");
+    temperatura_resultado.classList.add("d-none");
+    dias_section.classList.add("d-none");
+    dias_resultado.classList.add("d-none");
+    promedio_section.classList.add("d-none");
+    promedio_resultado.classList.add("d-none");
   } else {
     flowcharts_content.classList.add("d-none");
   }
@@ -124,9 +142,14 @@ operations_button.onclick = function () {
     operations_buttons.classList.remove("d-none");
     resultado.classList.remove("d-none");
     flowcharts_content.classList.add("d-none");
+    temperatura_section.classList.add("d-none");
+    temperatura_resultado.classList.add("d-none");
+    dias_section.classList.add("d-none");
+    dias_resultado.classList.add("d-none");
+    promedio_section.classList.add("d-none");
+    promedio_resultado.classList.add("d-none");
   } else {
     operations_section.classList.add("d-none");
-    flowcharts_content.classList.add("d-none");
     operations_buttons.classList.add("d-none");
     resultado.classList.add("d-none");
   }
@@ -210,5 +233,156 @@ function modulo(num1, num2) {
     if (isNaN(modulo) == false) {
       resultado.innerHTML = "El módulo es " + modulo;
     }
+  }
+}
+
+//Botón Temperatura
+temperatura_button.onclick = function () {
+  if (temperatura_section.classList.contains("d-none")) {
+    temperatura_section.classList.remove("d-none");
+    temperatura_resultado.classList.remove("d-none");
+    flowcharts_content.classList.add("d-none");
+    suma_flowchart.classList.add("d-none");
+    resta_flowchart.classList.add("d-none");
+    multiplicacion_flowchart.classList.add("d-none");
+    division_flowchart.classList.add("d-none");
+    modulo_flowchart.classList.add("d-none");
+    operations_section.classList.add("d-none");
+    operations_buttons.classList.add("d-none");
+    resultado.classList.add("d-none");
+    dias_section.classList.add("d-none");
+    dias_resultado.classList.add("d-none");
+    promedio_section.classList.add("d-none");
+    promedio_resultado.classList.add("d-none");
+  } else {
+    temperatura_section.classList.add("d-none");
+    temperatura_resultado.classList.add("d-none");
+  }
+};
+
+function aceptar_temperatura_button(temperatura) {
+  if (temperatura != "") {
+    temperatura = parseFloat(temperatura);
+    temperatura_Kelvin = temperatura + 273.15;
+    temperatura_Fahrenheit = (temperatura * 9) / 5 + 32;
+    temperatura_resultado.innerHTML =
+      temperatura +
+      "°C son equivalentes a " +
+      temperatura_Kelvin +
+      "°K y a " +
+      temperatura_Fahrenheit +
+      "°F";
+    temperatura_resultado.classList.remove("d-none");
+  } else {
+    temperatura_resultado.classList.add("d-none");
+  }
+}
+
+//Boton Dias
+dias_button.onclick = function () {
+  if (dias_section.classList.contains("d-none")) {
+    dias_section.classList.remove("d-none");
+    dias_resultado.classList.remove("d-none");
+    temperatura_section.classList.add("d-none");
+    temperatura_resultado.classList.add("d-none");
+    flowcharts_content.classList.add("d-none");
+    suma_flowchart.classList.add("d-none");
+    resta_flowchart.classList.add("d-none");
+    multiplicacion_flowchart.classList.add("d-none");
+    division_flowchart.classList.add("d-none");
+    modulo_flowchart.classList.add("d-none");
+    operations_section.classList.add("d-none");
+    operations_buttons.classList.add("d-none");
+    resultado.classList.add("d-none");
+    promedio_section.classList.add("d-none");
+    promedio_resultado.classList.add("d-none");
+  } else {
+    dias_section.classList.add("d-none");
+    dias_resultado.classList.add("d-none");
+  }
+};
+
+function aceptar_dias_button(dias) {
+  if (dias != "") {
+    dias = parseFloat(dias);
+    if (dias / 365 >= 1) {
+      años = Math.floor(dias / 365);
+      dias_despues_años = dias % 365;
+      if (dias_despues_años / 7 >= 1) {
+        semanas = Math.floor(dias_despues_años / 7);
+        dias_despues_semanas = dias_despues_años % 7;
+        dias_resultado.innerHTML =
+          dias +
+          " dias equivalen a " +
+          años +
+          " años, " +
+          semanas +
+          " semanas y " +
+          dias_despues_semanas +
+          " dias";
+      } else {
+        dias_resultado.innerHTML =
+          dias +
+          " dias equivalen a " +
+          años +
+          " años, " +
+          dias_despues_años +
+          " dias";
+      }
+    } else {
+      if (dias / 7 >= 1) {
+        semanas = Math.floor(dias / 7);
+        dias_despues_semanas = dias % 7;
+        dias_resultado.innerHTML =
+          dias +
+          " dias equivalen a " +
+          semanas +
+          " semanas y " +
+          dias_despues_semanas +
+          " dias";
+      } else {
+        dias_resultado.innerHTML = dias + " dias no alcanzan a ser una semana";
+      }
+    }
+  }
+}
+
+//Promedio
+promedio_button.onclick = function () {
+  if (promedio_section.classList.contains("d-none")) {
+    promedio_section.classList.remove("d-none");
+    promedio_resultado.classList.remove("d-none");
+    dias_section.classList.add("d-none");
+    dias_resultado.classList.add("d-none");
+    temperatura_section.classList.add("d-none");
+    temperatura_resultado.classList.add("d-none");
+    flowcharts_content.classList.add("d-none");
+    suma_flowchart.classList.add("d-none");
+    resta_flowchart.classList.add("d-none");
+    multiplicacion_flowchart.classList.add("d-none");
+    division_flowchart.classList.add("d-none");
+    modulo_flowchart.classList.add("d-none");
+    operations_section.classList.add("d-none");
+    operations_buttons.classList.add("d-none");
+    resultado.classList.add("d-none");
+  } else {
+    promedio_section.classList.add("d-none");
+    promedio_resultado.classList.add("d-none");
+  }
+};
+
+function aceptar_promedio_button(n1, n2, n3, n4, n5) {
+  if (n1 != "" && n2 != "" && n3 != "" && n4 != "" && n5 != "") {
+    n1 = parseFloat(n1);
+    n2 = parseFloat(n2);
+    n3 = parseFloat(n3);
+    n4 = parseFloat(n4);
+    n5 = parseFloat(n5);
+    var suma = n1 + n2 + n3 + n4 + n5;
+    var promedio = suma / 5;
+    promedio_resultado.innerHTML =
+      "La suma de los números es " + suma + " y su promedio es " + promedio;
+  } else {
+    promedio_resultado.innerHTML = "Ingrese los cinco números";
   }
 }
